139a5dee8e1cccf71bea5f5a3bf0e0a0344b5842
Ramsey Shehadeh <ramseys@doodleplex.com>
2014-11-29 15:51:58 -0500

    Add album art to compositions page, along with a lightbox that displays all elements of that art. Incorporate the Fancybox control for that purpose.

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -6,6 +6,11 @@ class CompositionsController < ApplicationController
 
   def show
     @comp = Composition.find(params[:id])
+    @associated_images = get_associated_images(@comp.Title)
+  end
+
+  def get_associated_images(title)
+    Dir["public/images/album-art/*"].entries.select { |name| name.index(/#{title}/i) }.map{ |name| name.sub("public/", "").sub("[", "%5B").sub("]", "%5D")}
   end
 
 end
