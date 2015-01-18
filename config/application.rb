48983ac5b01d9e9759f97c6ddf3f7aafaf242a4b
Ramsey Shehadeh <ramseys@doodleplex.com>
2015-01-18 16:28:20 -0500

    Another attempt to get images showing up in the asset pipeline in production: include all pngs and gigs in the config.assets.precompile.

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Robyn2
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+    # config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
   end
 end
