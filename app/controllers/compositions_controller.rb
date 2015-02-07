4c0d97974f3f859fae2991fae4cefdb3ff10c46b
Ramsey Shehadeh <ramseys@doodleplex.com>
2015-02-07 08:12:44 -0500

    Impose an explicit sort order on edition display in the album page.

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,5 +1,22 @@
 class CompositionsController < ApplicationController
 
+  # types of media, and the order in which they should appear
+  $MEDIA_TYPES = {
+    'CD'         => 0,
+    'Vinyl LP'   => 1,
+    'Vinyl 12"'  => 2,
+    'Vinyl 7"'   => 3,
+    'Cassette'   => 4,
+    'CD 3"'      => 5,
+    'CD Single'  => 6,
+    'DVD'        => 7,
+    'Fixel-disc' => 8,
+    'Videotape'  => 9,
+    'Other'      => 10
+  }
+
+  $MEDIA_TYPES.default = 10;
+
   def index
     
     if params[:search_type].present?
@@ -11,13 +28,34 @@ class CompositionsController < ApplicationController
   def show
 
     # get the requested album
-    @comp = Composition.find(params[:id])
+    comp = Composition.find(params[:id])
 
-    # get other editions of the same album
-    @other_editions = Composition.where(Title: @comp.Title).reject{|a| a.COMPID == @comp.COMPID}
+    # get all editions of the same album, sorting appropriately
+    @other_editions = Composition.where(Title: comp.Title).sort do |a, b| 
+
+      # first sort by medium
+      medium_order = $MEDIA_TYPES[a.Medium] <=> $MEDIA_TYPES[b.Medium]
+
+      # if the media match, sort the ones *without* notes higher
+      if medium_order == 0
+        if  (a.Comments.present? and b.Comments.nil?) 
+          medium_order = 1
+        elsif (a.Comments.nil? and b.Comments.present?) 
+          medium_order = -1
+        else 
+          medium_order = 0
+        end
+      end
+
+      medium_order
+
+    end
+
+    # separate out the first of the editions, it gets pride of place
+    @comp = @other_editions.shift
 
     # get album art (if any)
-    @associated_images = get_associated_images(@comp.Title)
+    @associated_images = get_associated_images(comp.Title)
 
   end
 
